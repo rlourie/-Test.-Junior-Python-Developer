56e6238df73fd84a41c7a753bc4ee30fcd5eac83
rlourie <super.sanek20142@yandex.ru>
2022-07-24 09:24:31 +0300

    Task_1__1

--- Task_1.py
+++ Task_1.py
@@ -2,19 +2,9 @@ def linear_search(array):
     return (array.index(0) if 0 in array else False)
 
 
-def binary_search(array, element=0):
-    i = 0
-    while i < len(array) and array[i] != element:
-        i += 1
-    if i < len(array):
-        return i
-    else:
-        return False
-
-
-input_array = '111111111111111111111111100000000'
+input_array = '11111111111100000000'
+b = input_array.find('10') + 1
+print(f"Функции Python: {b}")
 array = list(input_array)
 array = list(map(int, array))
-print(array)
-print(f'Линейный поиск (O(n)): {linear_search(array)}')
-print(f"Бинарный поиск (O(log(n))): {binary_search(array)}")
+print(f'Линейный поиск: {linear_search(array)}')
